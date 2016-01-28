803209e27d834347673b2028f08f4db8c00f3d39
Techblogogy <fedorbobylev@gmail.com>
2016-01-28 20:17:47 +0200

    Updated Verison Number

--- ttr-db.php
+++ ttr-db.php
@@ -3,7 +3,7 @@
 /*
  * Plugin Name: Tutor Database 
  * Description: Stores, searches tutors from database
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Fedor Bobylev
  * Author URI: http://techblogogy.tk/
  * GitHub Plugin URI: https://github.com/dev-tutors-ua/ttr-db
